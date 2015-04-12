d9828e2e77a0cc5dd27844e0057487ae8b2b2800
LogansUA <logansoleg@gmail.com>
2015-04-12 19:02:45 +0300

    Add wordpress install

--- avhs.sh
+++ avhs.sh
@@ -6,6 +6,14 @@ read -p "Server path (/var/www): " server
 read -p "Host path (/etc/hosts): " host
 read -p "IP host (127.0.0.1): " ip
 
+read -p "Do you want to create some type of project? [Y/N]: " yn
+
+case $yn in
+	("y" | "Y" | "Yes" | "YES")
+		read -p "Insert type of project: " type
+	;;
+esac
+
 # If project is not defined then use default
 if [ -z "$project" ];	then
 		project="new_project"
@@ -46,3 +54,26 @@ sudo sed -i "3i $ip\t$project" $host
 
 # Restart apache2
 sudo service apache2 restart
+
+function wordpressInstall { # Done
+  cd $server/$project
+
+  wget http://wordpress.org/latest.tar.gz
+
+  tar xvfz $server/$project/latest.tar.gz
+
+  mv $server/$project/wordpress/* $server/$project
+
+  rm $server/$project/latest.tar.gz
+  rm -rf $server/$project/wordpress
+
+  # mv $server/$project/wp-config-sample.php $server/$project/wp-config.php
+}
+
+if [ ! -z "$type" ]; then
+	case $type in
+	  ("Wordpress" | "wordpress" | "WordPress")
+			wordpressInstall
+	  ;;
+	esac
+fi
